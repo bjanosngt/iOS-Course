3237d3938e19e33d1ff71b1d734cf2de2c89e618
Janos <benjamin_janos@cable.comcast.com>
2016-08-05 12:06:26 -0600

    adding tapper

---
+++ tapper/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  tapper
+//
+//  Created by Janos, Benjamin on 6/7/16.
+//  Copyright © 2016 Janos, Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    // Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps++
+        updateTapsLbl()
+        
+        if isGameOver() {
+            restartGame()
+        }
+        
+    }
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapsLbl.hidden = false
+            tapBtn.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+            
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapsLbl.hidden = true
+        tapBtn.hidden = true
+    }
+
+    func isGameOver() -> Bool {
+        if currentTaps >= maxTaps {
+            return true
+        }
+        return false
+    }
+    
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+}
+
